13a2b11713983b3c9fe54981c0e2e2613f8e643a
Anuj Tiwari <anujtiwari720ya@gmail.com>
2023-07-03 19:17:39 +0530

    snake

---
+++ snake and ladders/myjs.js
@@ -0,0 +1,29 @@
+//game constants and variables
+let direction = {x:0, y:0};
+const foodsound = new Audio('eating.mp3');
+const gameoversound = new Audio('gameo.mp3');
+const movingsound = new Audio('moving.mp3');
+const backgroundmusic = new Audio('music.mp3');
+
+//game funtions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//main logic
+window.requestAnimationFrame(main)
